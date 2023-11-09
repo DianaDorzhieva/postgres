2fb66889a8e4d7fd5a715ec6064b996f53d45454
diana <diana-safina-2017@mail.ru>
2023-11-09 19:14:10 +0500

    HW1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,31 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+ employee_id int PRIMARY KEY,
+ first_name varchar(100) NOT NULL,
+ last_name varchar(100) NOT NULL,
+ title varchar(300) NOT NULL,
+ birth_date varchar(300) NOT NULL,
+ notes text
+);
+
+
+CREATE TABLE customers
+(
+  customer_id text PRIMARY KEY,
+  company_name text NOT NULL,
+  contact_name varchar(300) NOT NULL
+);
+
+CREATE TABLE orders
+(
+  order_id int PRIMARY KEY,
+  custom_id text REFERENCES customers(customer_id) NOT NULL,
+  employ_id int REFERENCES employees(employee_id) NOT NULL,
+  order_date text,
+  ship_city text
+)
+
+
+SELECT * from orders
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='12345'
+)
+cur = conn.cursor()
+"""Добавляем данные в таблицу customers"""
+file_way1 = "../src/customers_data.csv"
+with open(file_way1) as file:
+    words = csv.DictReader(file)
+    for word in words:
+        cur.execute('INSERT INTO customers VALUES (%s,%s,%s)',
+                    (word["customer_id"], word["company_name"], word["contact_name"]))
+
+"""Добавляем данные в таблицу employees"""
+file_way2 = "../src/employees_data.csv"
+with open(file_way2) as file:
+    words = csv.DictReader(file)
+    for word in words:
+        cur.execute('INSERT INTO employees VALUES (%s,%s,%s,%s,%s,%s)',
+                    (word['employee_id'], word['first_name'], word['last_name'],
+                     word['title'], word['birth_date'], word['notes']))
+
+"""Добавляем данные в таблицу  orders"""
+file_way3 = "../src/orders_data.csv"
+with open(file_way3) as file:
+    words = csv.DictReader(file)
+    for word in words:
+        cur.execute('INSERT INTO orders VALUES (%s,%s,%s,%s,%s)',
+                    (word["order_id"], word["customer_id"], word["employee_id"],
+                     word["order_date"], word["ship_city"]))
+
+conn.commit()
+cur.close()
+conn.close()
